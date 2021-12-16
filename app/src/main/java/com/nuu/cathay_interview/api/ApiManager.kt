4fdb48a051f134acfa5a099cf78b8592828288f0
ci-charlie.lin <charlie.lin@cloud-interactive.com>
2021-12-16 18:38:25 +0800

    優化api方法

--- app/src/main/java/com/nuu/cathay_interview/api/ApiManager.kt
+++ app/src/main/java/com/nuu/cathay_interview/api/ApiManager.kt
@@ -1,24 +1,23 @@
 package com.nuu.cathay_interview.api
 
+import com.nuu.cathay_interview.constant.AppConstant
 import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 class ApiManager private constructor() {
-    private val retrofit: Retrofit
-    private val okHttpClient = OkHttpClient()
-
-    init {
-        retrofit = Retrofit.Builder()
-            .baseUrl("https://data.taipei")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-    }
 
     companion object {
-        private val manager = ApiManager()
-        val client: Retrofit
-            get() = manager.retrofit
+        private val okHttpClient = OkHttpClient()
+        val client by lazy { invoke(AppConstant.baseUrl) }
+        val client2 by lazy { invoke(AppConstant.baseUrl2) }
+
+        operator fun invoke(baseUrl: String): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+        }
     }
 }
---
+++ app/src/main/java/com/nuu/cathay_interview/constant/AppConstant.kt
@@ -0,0 +1,9 @@
+package com.nuu.cathay_interview.constant
+
+class AppConstant {
+
+    companion object {
+        const val baseUrl = "https://www.reddit.com/"
+        const val baseUrl2 = "https://www.reddit.com/"
+    }
+}
