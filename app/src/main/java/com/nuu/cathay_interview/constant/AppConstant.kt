be29ca79a135cedce6e3b9b3a346f85393f4e6d0
ci-charlie.lin <charlie.lin@cloud-interactive.com>
2022-02-25 16:08:06 +0800

    Revert "優化api方法"
    This reverts commit 4fdb48a051f134acfa5a099cf78b8592828288f0.

--- app/src/main/java/com/nuu/cathay_interview/constant/AppConstant.kt
+++
@@ -1,9 +0,0 @@
-package com.nuu.cathay_interview.constant
-
-class AppConstant {
-
-    companion object {
-        const val baseUrl = "https://www.reddit.com/"
-        const val baseUrl2 = "https://www.reddit.com/"
-    }
-}
